2596184e994ec7d98a3f6383304b3a9eba806b04
Valledor099 <alumno27.valledor.agustin@ipm.edu.ar>
2025-11-06 22:57:16 -0300

    Se agregaron elementos a la sidebar

--- menu.js
+++ menu.js
@@ -1,6 +1,7 @@
 const Menu = document.querySelector('.menu');
 const offScreenMenu = document.querySelector('.off-screen-menu');
 const exit = document.querySelector('.salir');
+const Opcion1 = document.querySelector('#op1');
 
 Menu.addEventListener('click', () => {
     offScreenMenu.classList.toggle('active');
@@ -10,5 +11,8 @@ exit.addEventListener('click', () => {
   offScreenMenu.classList.remove('active');
 });
 
+Opcion1.addEventListener('click', () => {
+  offScreenMenu.classList.remove('active');
+});
 
 
